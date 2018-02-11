1a3aa3fe5ea38898b8580505a64d6eb1ea88b578
Angela Deslandes <ang.deslandes@gmail.com>
2018-02-10 19:58:05 -0700

    worked on jquery functionality of appending input to output

--- script.js
+++ script.js
@@ -3,26 +3,38 @@ var List = document.querySelector('.secondSection ul');
 var form = document.forms['inputForm'];
 
 $submit.on('click',function(e){
-var $title = $('.titleInput').value;
-var $body = $('.bodyInput').value;
+// var $title = $('.titleInput').value;
+// var $body = $('.bodyInput').value;
 e.preventDefault();
-form.reset();
 create();
 create2();
+form.reset();
 })
 
 function create(){
-  var li= document.createElement('li');
+  var li = document.createElement('li');
+  var inputTitle = $('.titleInput').val();
+  var inputBody = $('.bodyInput').val();
+
    li.classList.add('newIdeas');
    List.appendChild(li); 
+
+  var newTitle = `<h3> ${inputTitle} </h3>`;
+  var newBody =  `<p> ${inputBody} </p>`;
+   
+    $('ul').append(newTitle);
+    $('ul').append(newBody);
+
+    console.log('hello');
 }
 
 function create2(){
-   var t = $("<div class = 'firstLine'><h3 class='quality'>Hola</h3><img src='images/delete.svg' class='deleteButton'></div>");
+
+  var t = $("<div class = 'firstLine'><h3 class='quality'></h3><img src='images/delete.svg' class='deleteButton'></div>");
   $('li').append(t);
-  var t2 = $("<p class='example-body'>Hola</p>");
+  var t2 = $("<p class='example-body'></p>");
   $('li').append(t2);
-  var t3 = $("<div class = 'lineThree'><img src='images/downvote.svg' class='downArrow'><img src='images/upvote.svg' class='upArrow'><p class='quality'>hola</p></div>");
+  var t3 = $("<div class = 'lineThree'><img src='images/downvote.svg' class='downArrow'><img src='images/upvote.svg' class='upArrow'><p class='quality'></p></div>");
   $('li').append(t3);
 
 }
