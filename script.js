fdd751f286ebc5e27c32acfa2c067a606334bd9a
sospinar21 <sospinar21@gmail.com>
2018-02-10 18:14:13 -0700

    Create new li and all the elements inside the idea box, save button working, on click it creates new article

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
 var $submit = $('.saveButton');
-var $ideaList = $('ul');
+var List = document.querySelector('.secondSection ul');
+var form = document.forms['inputForm'];
 
 $submit.on('click',function(e){
 var $title = $('.titleInput').value;
 var $body = $('.bodyInput').value;
 e.preventDefault();
+form.reset();
+create();
+create2();
 })
 
+function create(){
+  var li= document.createElement('li');
+   li.classList.add('newIdeas');
+   List.appendChild(li); 
+}
+
+function create2(){
+   var t = $("<div class = 'firstLine'><h3 class='quality'>Hola</h3><img src='images/delete.svg' class='deleteButton'></div>");
+  $('li').append(t);
+  var t2 = $("<p class='example-body'>Hola</p>");
+  $('li').append(t2);
+  var t3 = $("<div class = 'lineThree'><img src='images/downvote.svg' class='downArrow'><img src='images/upvote.svg' class='upArrow'><p class='quality'>hola</p></div>");
+  $('li').append(t3);
+
+}
+//   var title= document.createElement('h3').addClass(h3);
+//   var $text = document.createElement('p').addClass(example-body);
+//   var $linkDelete= document.createElement('img').addClass(deleteButton);
+//   var buttonDown = document.createElement('img').addClass(downArrow);
+//   var linkUp = document.createElement('img').addClass(upArrow);
+//   var quality = document.createElement('p').addClass(quality);
 
 
-// function newIdeas(title, text,buttonUp,buttonDown,quality,close){
-//   this.title = title;
-//   this.text = text;
-//   this.buttonUp = buttonUp;
-//   this.buttonDown = buttonDown;
-//   this.quality = quality;
-//   this.close = close;
-// }
-
-// CREATING ELEMENTS
 
