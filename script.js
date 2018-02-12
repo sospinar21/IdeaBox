7cd44e9689d1b9a04d5e0a018528bea92c4df9c9
Steph <31945650+sospinar21@users.noreply.github.com>
2018-02-11 17:26:25 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,57 +5,30 @@ var list = document.querySelector('.secondSection ul');
 var titleInput = document.querySelector('.titleInput').value
 var bodyInput= document.querySelector('.bodyInput').value;
 
+// $(document).ready(radioSelection)
+
+// clone box with the user's input
 form.addEventListener('submit',function(e){
 e.preventDefault();
-  // desable()
  cloneIdea();
  form.reset();
 });
 
+
+// clone box
 function cloneIdea(){
 var boxCopy = boxTemplate.cloneNode(true);
-boxCopy.id = "";
-boxCopy.querySelector('h3').innerText= document.querySelector('.titleInput').value;
+boxCopy.id = '';
+boxCopy.querySelector('textarea').innerText= document.querySelector('.titleInput').value;
 boxCopy.querySelector('.example-body').innerText= document.querySelector('.bodyInput').value;
+boxCopy.querySelector('select').value= $("input[name ='quality']:checked").val();
 list.appendChild(boxCopy);
 var deleteButton = boxCopy.querySelector('.deleteButton');
 deleteButton.addEventListener('click', deleteIdea)
 }
 
-$submit.on('click',function(e){
-e.preventDefault();
-createInput();
-form.reset();
-})
-
-function createInput(){
-  var li = document.createElement('li');
-  var newTitle = document.querySelector('.titleInput').value;
-  var newBody = document.querySelector('.bodyInput').value;
-
-  li.classList.add('newIdeas');
-  List.appendChild(li); 
-
-  var inputTitle = $('<h3>' + newTitle + '</h3><img src="images/delete.svg" class="deleteButton" style="display: inline-block; float: right;">');
-  $('li').append(inputTitle);
-  var inputBody = $('<p class="example-body">' + newBody + '</p>');
-  $('li').append(inputBody);
-  var iconPic = $('<div class = "firstLine"><img src="images/upvote.svg" class="upArrow" style="padding-right: 30px; margin-bottom: 20px;"><img src="images/downvote.svg" class="downArrow"></div');
-  $('li').append(iconPic);
-
-  console.log(newTitle, newBody);
-}
-
-//   var title= document.createElement('h3').addClass(h3);
-//   var $text = document.createElement('p').addClass(example-body);
-//   var $linkDelete= document.createElement('img').addClass(deleteButton);
-//   var buttonDown = document.createElement('img').addClass(downArrow);
-//   var linkUp = document.createElement('img').addClass(upArrow);
-//   var quality = document.createElement('p').addClass(quality);
-
-
+// delete
 function deleteIdea(ev){
  var box = ev.target.closest('.newIdeas');
  list.removeChild(box);
-
 }
